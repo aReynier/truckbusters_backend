c5da602d4720005159ba55dd0d528dad62c073d4
Aurore Reynier <reynier.aurore@gmail.com>
2024-04-12 11:43:56 +0200

    connexion with MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 const port = 3002
 const version = "v1"
+import dotenv from 'dotenv';
+dotenv.config();
 
 import router from './routes/appointment.js';
 
@@ -10,3 +13,7 @@ app.use(`/api/${version}/appointment`, router)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.iajdy8a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+  .then(() => console.log('MongoDB connexion success !'))
+  .catch(() => console.log('MongoDB connexion fail !'));
