cdeaadf3ffada986c13461746037c142c0421877
Aurore Reynier <reynier.aurore@gmail.com>
2024-04-26 09:51:39 +0200

    add some security libraries

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
 
 import appointmentRouter from './routes/appointment.js';
 import informationRouter from './routes/information.js';
@@ -12,6 +14,16 @@ import swaggerDocs from './swagger.js'
 const app = express();
 const port = 3002
 const version = "v1"
+
+app.use(helmet());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Cross-Origin-Resource-Policy', 'same-site'); //fix helmet not same origin error
+  next();
+});
 
 app.use(cors())
 
@@ -27,6 +39,8 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PA
   .catch(() => console.log('MongoDB connexion fail !'));
 
 app.use(bodyParser.json());
+
+app.use(mongoSanitize());
 
 app.use(`/api/${version}/appointment`, appointmentRouter)
 app.use(`/api/${version}/information`, informationRouter)
