da20d28be5c557a0536ac84f94a1fd7c958a6629
Aurore Reynier <reynier.aurore@gmail.com>
2026-04-06 15:38:14 +0200

    chore: add supertest and first basic test

---
+++ controllers/tests/appointment.test.js
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest';
+import request from 'supertest'; 
+import app from '../../server.js'
+
+const version = "v1";
+
+describe(`POST /appointment`, () => {
+  it('Must send error message', async () => {
+    const newAppointment = {
+      firstname: 'Test User',
+    };
+
+    const response = await request(app)
+      .post(`/api/${version}/appointment`)
+      .send(newAppointment)
+      .set('Accept', 'application/json');
+
+    expect(response.status).toBe(400);
+  })
+});
