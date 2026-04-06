58458c70e597424ca1c54cb332c25723e759e291
Aurore Reynier <reynier.aurore@gmail.com>
2026-04-06 13:01:31 +0200

    refactor(controllers): apply prettier

--- controllers/sendSecretaryEmail.js
+++ controllers/sendSecretaryEmail.js
@@ -1,6 +1,14 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-const sendSecretaryEmail = (moment, email, name, phone, firstname, lastname, driverPhone) => {
+const sendSecretaryEmail = (
+  moment,
+  email,
+  name,
+  phone,
+  firstname,
+  lastname,
+  driverPhone,
+) => {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
     host: "smtp.gmail.com",
@@ -11,14 +19,14 @@ const sendSecretaryEmail = (moment, email, name, phone, firstname, lastname, dri
       pass: process.env.SMTP_PASSWORD,
     },
   });
-  
+
   const mailOptions = {
     from: "your_email@gmail.com",
     to: "reynier.aurore@gmail.com",
     subject: "Nouvelle réservation",
-    text: `Bonjour Janine, nouvelle réservation le ${new Date(moment.toLocaleString('fr-FR', { timeZone: 'UTC' }))} (compter deux heures de plus pour l'heure française) de l'enteprise ${name} dont l'email est ${email}. Leur conducteur se nomme ${lastname} ${firstname}. Les coordonées supplémentaires si il y en a: numéro de téléphone de l'entreprise: ${phone}, numéro de téléphone du chauffeur ${driverPhone}. Passez une bonne journée`,
+    text: `Bonjour Janine, nouvelle réservation le ${new Date(moment.toLocaleString("fr-FR", { timeZone: "UTC" }))} (compter deux heures de plus pour l'heure française) de l'enteprise ${name} dont l'email est ${email}. Leur conducteur se nomme ${lastname} ${firstname}. Les coordonées supplémentaires si il y en a: numéro de téléphone de l'entreprise: ${phone}, numéro de téléphone du chauffeur ${driverPhone}. Passez une bonne journée`,
   };
-  
+
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error("Error sending email: ", error);
@@ -26,6 +34,6 @@ const sendSecretaryEmail = (moment, email, name, phone, firstname, lastname, dri
       console.log("Email sent: ", info.response);
     }
   });
-}
+};
 
-export default sendSecretaryEmail
+export default sendSecretaryEmail;
